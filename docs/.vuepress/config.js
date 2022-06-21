4f2f1f99c68a731d967040912666f316fac855b7
Ronny Mees <ronny.mees@vives.be>
2022-06-21 17:36:27 +0200

    adjust some vuepress stuff

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,16 +27,7 @@ module.exports = {
   serviceWorker: true,
   plugins: [
     ['vuepress-plugin-zooming', {
-      // selector for images that you want to be zoomable
-      // default: '.content img'
       selector: 'img',
-
-      // make images zoomable with delay after entering a page
-      // default: 500
-      // delay: 1000,
-
-      // options of zooming
-      // default: {}
       options: {
         bgColor: 'black',
         zIndex: 10000,
